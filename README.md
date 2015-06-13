ebf6598f6a074ec807ac23f7e20e331abb40d6d0
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2015-06-13 18:07:12 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-=== Wordpress Picture Element ===
-Contributors: eclev91
-Tags: thumbnails
-Requires at least: 4.0
-Tested up to: 4.2.2
-Stable tag: 4.2.2
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+# Wordpress Picture Element #
+**Contributors:** eclev91
+  
+**Tags:** thumbnails
+  
+**Requires at least:** 4.0
+  
+**Tested up to:** 4.2.2
+  
+**Stable tag:** 4.2.2
+  
+**License:** GPLv2 or later
+  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html
+  
 
 Get a picture element instead of an image tag for responsive images on your WordPress site.
 
-== Description ==
+## Description ##
 
 This plugin introduces a couple of nifty functions to replace `the_post_thumbnail()` & `get_the_post_thumbnail()` in exchange for the responsive `picture` element. Use `get_the_post_picture($fallback_image_size, array($breakpoint_1=>$image_size_1, $breakpoint_2=>$image_size_2[, $id]);`
 
@@ -17,44 +24,45 @@ For example:
 
 ```<?php echo get_the_post_picture('single-featured-sm', array('(min-width:768px)'=>'single-featured-md', '(min-width:992px)'=>'single-featured-lg', '(min-width:1200px)'=>'single-featured')); ?>```
 
-While this says 4.0 for minimum version, I would wager it'll be fine on most older installations.
-
-== Installation ==
+## Installation ##
 
 1. Install via the WordPress Plugin directory or upload this to your plugins folder.
 
 2. Activate from the Plugins screen on your Dashboard.
 
-== Frequently Asked Questions ==
+## Frequently Asked Questions ##
 
-= What? =
+### What? ###
 
 `the_post_picture()` and `get_the_post_picture()`. See the description for an example.
 
-= Why? =
+### Why? ###
 
 This is really a bigger question than the scope of this plugin, but [see here](http://code.tutsplus.com/tutorials/better-responsive-images-with-the-picture-element--net-36583).
 
 Oh, you weren't talking about the picture element as a whole. There exists at least one plugin that will out-of-the-box replace all `<img>` tags with "picture" elements and set breakpoints based on the image sizes. I don't think that's enough control. This plugin gives you control over A. Which elements are replaced and B. What your breakpoints are. Though I see the benefits of the "automatic" aspect, and may implement this at a later time.
 
-= Does the order of my sizes array matter? =
+### Does the order of my sizes array matter? ###
 
 Yup. Pass them smallest image to largest.
 
-= Browser Support? =
+### Browser Support? ###
 
 The plugin includes [PictureFill.js](http://scottjehl.github.io/picturefill/). This should polyfill most browsers. If you have an issue, please report it!
 
-= Can I use my custom image sizes? =
+### Can I use my custom image sizes? ###
 
 Yessir.
 
-= Can I contribute? =
+### Can I contribute? ###
 
 PRs are welcome! [See GitHub.](https://github.com/ethanclevenger91/WordpressPictureElement)
 
 
-== Changelog ==
+## Screenshots ##
 
-= 1.0 =
+
+## Changelog ##
+
+### 1.0 ###
 * Initial launch!
---
+++ wordpress-picture-element.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Plugin Name: Wordpress Picture Elements
+ * Version: 1.0.0
+ * Description: Add functions for using the <picture> element for WP featured images
+ */
+
+class WPPictureElement {
+	function __construct() {
+		add_action('wp_enqueue_scripts', array($this, 'wp_enqueue_scripts'));
+	}
+
+	/**
+	 * Get the picture element
+	 * @param string $default_image_size
+	 * @param array $sizes 
+	 * @param int $post_id 
+	 * @return string
+	 */
+	function get_picture_element($default_image_size = 'thumbnail', $sizes=false, $post_id) {
+		if(!$sizes) {
+			return get_the_post_thumbnail($id, $default_image_size);
+		}
+		if(has_post_thumbnail($post_id)) {
+			wp_enqueue_script('wpe_picturefill');
+			$thumbnail_id = get_post_thumbnail_id($post_id);
+			//Add video tags for IE9? http://scottjehl.github.io/picturefill/#ie9
+			return '<picture>'
+					. '<!--[if IE 9]><video style="display: none;"><![endif]-->'
+		            . self::get_picture_srcs( $thumbnail_id, $sizes )
+		            . '<!--[if IE 9]></video><![endif]-->'
+		            . '<img srcset="' . wp_get_attachment_image_src( $thumbnail_id, $default_image_size)[0] . '" alt="' . self::get_img_alt( $thumbnail_id ) . '">
+		        </picture>';
+		}
+		return '';
+	}
+
+	/**
+	 * Enqueue picturefill
+	 * @return null
+	 */
+	function wp_enqueue_scripts() {
+		wp_register_script('wpe_picturefill', plugins_url('assets/js/picturefill.min.js', __FILE__ ), array(), '1.0.0', true);
+	}
+
+	/**
+	 * Grab the image's alt text meta
+	 * @param int $attachment_id 
+	 * @return string
+	 */
+	function get_img_alt( $attachment_id ) {
+	    $img_alt = trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) );
+	    return $img_alt;
+	}
+
+	/**
+	 * Get the actual src tag markup
+	 * @param int $post_id 
+	 * @param array $sizes 
+	 * @return string
+	 */
+	function get_picture_srcs( $thumbnail_id, $sizes ) {
+		global $_wp_additional_image_sizes;
+		$newSizes = array();
+        $intermediate_image_sizes = $sizes;
+
+        // Create the full array with sizes and crop info
+        foreach( $intermediate_image_sizes as $breakpoint => $_size ) {
+
+        	//if it's a default image size
+            if ( in_array( $_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
+            	$newSizes[ $_size ][ 'breakpoint' ] = $breakpoint;
+                $newSizes[ $_size ][ 'width' ] = get_option( $_size . '_size_w' );
+                $newSizes[ $_size ][ 'height' ] = get_option( $_size . '_size_h' );
+                $newSizes[ $_size ][ 'crop' ] = (bool) get_option( $_size . '_crop' );
+
+        	//otherwise...
+            } elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+                $newSizes[ $_size ] = array( 
+                	'breakpoint' => $breakpoint,
+                    'width' => $_wp_additional_image_sizes[ $_size ][ 'width' ],
+                    'height' => $_wp_additional_image_sizes[ $_size ][ 'height' ],
+                    'crop' =>  $_wp_additional_image_sizes[ $_size ][ 'crop' ]
+                );
+            } elseif ( $_size == 'full' ) {
+            	$newSizes[ $_size ] = array(
+            		'breakpoint' => $breakpoint,
+            		'width' => 'auto',
+            		'height' => 'auto',
+            		'crop' => false
+            	);
+            }
+    	}
+
+	    $arr = array();
+	    foreach ( $newSizes as $size => $details ) {
+	        $image_src = wp_get_attachment_image_src( $thumbnail_id, $size );
+	        $arr[] = '<source srcset="'. $image_src[0] . '" media="'.$details['breakpoint'].'">';
+	    }
+	    return implode( array_reverse ( $arr ) );
+	}
+}
+$WPPictureElement = new WPPictureElement();
+
+/**
+ * Global alternative to the_post_thumbnail()
+ * @param string $default_image_size 
+ * @param array $sizes 
+ * @param int $post_id 
+ * @return null
+ */
+function the_post_picture($default_image_size = 'thumbnail', $sizes=false, $post_id='') {
+	if($post_id == '') {
+		global $post;
+		$post_id = $post->ID;
+	}
+	echo get_the_post_picture($default_image_size, $sizes, $post_id);
+}
+
+/**
+ * Global alternative to get_the_post_thumbnail
+ * @param string $default_image_size
+ * @param array $sizes 
+ * @param int $post_id 
+ * @return string
+ */
+function get_the_post_picture($default_image_size = 'thumbnail', $sizes=false, $post_id='') {
+	if($post_id == '') {
+		global $post;
+		$post_id = $post->ID;
+	}
+	$post_id = get_post_thumbnail_id($post_id);
+	return get_picture_element($default_image_size, $sizes, $post_id);
+}
